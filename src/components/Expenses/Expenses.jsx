5fc1094a595de5eb6461252908402ecabca34942
100ildWr <>
2022-12-04 10:18:24 +0000

    added no expenses message

---
+++ src/components/Expenses/Expenses.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
+import './Expenses.css';
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  };
+
+    const filteredExpenses=props.items.filter(expense=>{
+       return expense.date.getFullYear().toString()===filteredYear ;
+    });
+
+    let ExpensesContent=<p>No Expenses Found</p>
+    if(filteredExpenses.length>0){
+        ExpensesContent=filteredExpenses.map(expense=>(
+            <ExpenseItem 
+            key={expense.id}
+            title={expense.title} 
+            amount={expense.amount} 
+            date={expense.date}/>
+            ));
+    }
+
+  return (
+    <div>
+      <Card className='expenses'>
+        <ExpensesFilter 
+            selected={filteredYear} 
+            onChangeFilter={filterChangeHandler} />
+        {ExpensesContent}
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
